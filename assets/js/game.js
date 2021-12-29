a392bd7dd61dc7748d50f50a83be524aa3eeec67
Jonathan W <jwilliams12117@gmail.com>
2021-12-29 06:43:14 -0700

    Added loop to indicate rounds and display loss condition with game over

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,4 @@
-var playerName = window.prompt("What is your robot's name?");
+var playerName = window.prompt("Welcome to Robot Gladiators! What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
@@ -37,55 +37,64 @@ var fight = function(enemyName) {
             //Message upon confirm
             if (confirmSkip) {
                 window.alert("Skipped fight.  Subtracting 2 coins from inventory.");
-                playerMoney -= 2;
+                playerMoney -= 10;
                 console.log("player money: ", playerMoney);
                 break;
             }
         }
 
         //Evaluation if player chooses to fight
-        if (promptFight === "fight" || promptFight === "FIGHT") {
 
-            //Player's turn to attack enemy
-            enemyHealth -= playerAttack;
-            console.log("Enemy received", playerAttack, " damage from player.  New enemy HP: ", enemyHealth);
+        //Player's turn to attack enemy
+        enemyHealth -= playerAttack;
+        console.log("Enemy received", playerAttack, " damage from player.  New enemy HP: ", enemyHealth);
 
-            //Check enemy HP
-            if (enemyHealth <= 0) {
-                window.alert(enemyName + " has died!");
-                break;
-            }
-
-            else {
-                window.alert(enemyName + " still has " + enemyHealth + " HP left.");
-            }
-
-            //Enemy's turn to attack player
-            playerHealth -= enemyAttack;
-            console.log("Player received", enemyAttack, " damage from enemy.  New player HP: ", playerHealth);
-        
-            //Check player HP
-            if (playerHealth <= 0) {
-                window.alert(playerName + " has died!");
-                break;
-            }
-        
-            else {
-                window.alert(playerName + " still has " + playerHealth + " HP left.");
-            }
-
+        //Check enemy HP
+        if (enemyHealth <= 0) {
+            window.alert(enemyName + " has died!");
+            playerMoney += 20;
+            break;
+        }
+        else {
+            window.alert(enemyName + " still has " + enemyHealth + " HP left.");
         }
 
-        //Input validation for invalid inputs
+        //Enemy's turn to attack player
+        playerHealth -= enemyAttack;
+        console.log("Player received", enemyAttack, " damage from enemy.  New player HP: ", playerHealth);
+    
+        //Check player HP
+        if (playerHealth <= 0) {
+            window.alert(playerName + " has died!");
+            break;
+        }  
         else {
-            window.alert("Invalid option.")
+            window.alert(playerName + " still has " + playerHealth + " HP left.");
         }   
     }
-
   };
 
-
+//For loop to execute the fight function for each each enemy until dead or skipped
 for (i = 0; i < enemyNames.length; i++) {
-    enemyHealth = 50;
-    fight(enemyNames[i]);
+
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! - Round "+(i + 1)+"!");
+
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50;
+
+        // use debugger to pause script from running and check what's going on at that moment in the code
+        // debugger;
+
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+      }
 }
